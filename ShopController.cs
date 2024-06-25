2950076244d26b5cb7c19ae836db91c489166ea9
Charles d'Ansembourg <50960743+Eflox@users.noreply.github.com>
2024-06-25 15:52:38 +0100

    WIP

---
+++ ShopController.cs
@@ -0,0 +1,15 @@
+/*
+ * ShopController.cs
+ * Script Author: Charles d'Ansembourg
+ * Creation Date: 25/06/2024
+ * Contact: c.dansembourg@icloud.com
+ */
+
+using UnityEngine;
+
+namespace Brotato_Clone
+{
+	public class ShopController : MonoBehaviour
+	{
+	}
+}
