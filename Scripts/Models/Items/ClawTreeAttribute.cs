8ce45656e8ba74dd9d2da879aa9fb0d80c3fbaa6
Charles d'Ansembourg <50960743+Eflox@users.noreply.github.com>
2024-06-27 00:04:47 +0100

    WIP

---
+++ Scripts/Models/Items/ClawTreeAttribute.cs
@@ -0,0 +1,15 @@
+/*
+     * ClawTreeAttribute.cs
+     * Script Author: Charles d'Ansembourg
+     * Creation Date: 27/06/2024
+     * Contact: c.dansembourg@icloud.com
+     */
+
+    using Brotato_Clone.Interfaces;
+
+    namespace Brotato_Clone.Models
+    {
+        public class ClawTreeAttribute : IAttribute
+        {
+        }
+    }
---
+++ Scripts/Models/Items/ScopeAttribute.cs
@@ -0,0 +1,15 @@
+/*
+     * ScopeAttribute.cs
+     * Script Author: Charles d'Ansembourg
+     * Creation Date: 27/06/2024
+     * Contact: c.dansembourg@icloud.com
+     */
+
+    using Brotato_Clone.Interfaces;
+
+    namespace Brotato_Clone.Models
+    {
+        public class ScopeAttribute : IAttribute
+        {
+        }
+    }
