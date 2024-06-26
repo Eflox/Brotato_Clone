2222592453d30d249c11716b36e0774610954515
Charles d'Ansembourg <50960743+Eflox@users.noreply.github.com>
2024-06-26 10:40:21 +0100

    WIP

---
+++ Scripts/Models/Character.cs
@@ -0,0 +1,15 @@
+/*
+ * Character.cs
+ * Script Author: Charles d'Ansembourg
+ * Creation Date: 26/06/2024
+ * Contact: c.dansembourg@icloud.com
+ */
+
+using UnityEngine;
+
+namespace Brotato_Clone
+{
+	public class Character : MonoBehaviour
+	{
+	}
+}
---
+++ Scripts/Models/Items/AlienBabyAttribute.cs
@@ -0,0 +1,24 @@
+/*
+ * AlienBabyAttribute.cs
+ * Script Author: Charles d'Ansembourg
+ * Creation Date: 28/05/2024
+ * Contact: c.dansembourg@icloud.com
+ */
+
+using Brotato_Clone.Interfaces;
+
+namespace Brotato_Clone.Models
+{
+    public class AlienBabyAttribute : IAttribute
+    {
+        [Stat(operation: StatOperation.Add)]
+        public readonly int EnemySpeed = 8;
+
+        [Stat(operation: StatOperation.Add)]
+        public readonly int MaxHP = 15;
+
+        public int BasePrice => 80;
+
+        public int Limit => 0;
+    }
+}
