97d1645869049cc15029c8e89c05fb8ad6524f84
Charles d'Ansembourg <50960743+Eflox@users.noreply.github.com>
2024-07-21 16:31:19 +0100

    WIP

---
+++ ItemAttributesController.cs
@@ -0,0 +1,15 @@
+/*
+ * ItemAttributesController.cs
+ * Script Author: Charles d'Ansembourg
+ * Creation Date: 21/07/2024
+ * Contact: c.dansembourg@icloud.com
+ */
+
+using UnityEngine;
+
+namespace Brotato_Clone
+{
+	public class ItemAttributesController : MonoBehaviour
+	{
+	}
+}
